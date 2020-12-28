25b70c8bf7888f0b4d269722f8027d0c38c2231a
johnosbb <johnos37@yahoo.com>
2020-12-28 10:31:38 +0000

    Initial Commit

---
+++ create_respository.sh
@@ -0,0 +1,7 @@
+echo "# ElectronNewsApp" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/johnosbb/ElectronNewsApp.git
+git push -u origin main
